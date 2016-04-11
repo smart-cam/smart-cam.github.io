f411adf926283c812f7d4839b41bde0d1f586705
Juanjo Carin <juanjose.carin@ischool.berkeley.edu>
2016-04-11 13:55:46 -0700

    Template

--- js/contact_me.js
+++ js/contact_me.js
@@ -1,6 +1,6 @@
 $(function() {
 
-    $("input,textarea").jqBootstrapValidation({
+    $("#contactForm input,#contactForm textarea").jqBootstrapValidation({
         preventSubmit: true,
         submitError: function($form, event, errors) {
             // additional error messages or events
